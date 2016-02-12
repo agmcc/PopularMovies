c8f6422e139c554fbf0e2df031c7a19771234d86
agmcc <agmcc@users.noreply.github.com>
2016-02-12 18:00:37 +0000

    Test basic Picasso functionality

--- app/src/main/java/com/example/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/popularmovies/MoviesFragment.java
@@ -6,9 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MoviesFragment extends Fragment {
 
+    private final String LOG_TAG = MoviesFragment.class.getSimpleName();
 
     public MoviesFragment() {
     }
@@ -16,7 +20,15 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+        //test picasso image loading
+        ImageView imageView = (ImageView) rootView
+                .findViewById(R.id.testImageView);
+        Picasso.with(getContext())
+                .load("https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/11693880_904702142931018_4645832345442603442_n.jpg?oh=1e88cc598187c1793a0b1bbf3731b437&oe=5727AE07")
+                .into(imageView);
+        //
+        return rootView;
     }
 
 }
